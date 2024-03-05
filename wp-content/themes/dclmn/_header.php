8d06116590db7ee75a1b46e87699e85a0297790a
msteel <msteel@napco.com>
2024-03-04 20:23:27 -0500

    WP

---
+++ wp-content/themes/dclmn/_header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>sss</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+
+<body>
+    <div id="wrapper">
+        <img src="images/dclm.webp">
+        <?php include '_nav.php' ?>
