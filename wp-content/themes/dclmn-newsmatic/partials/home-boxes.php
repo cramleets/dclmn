b86ba7d344ce983be797bd594a56979551ff0dbd
msteel <msteel@napco.com>
2025-10-05 19:04:29 -0400

    Menu icon now svg.

--- wp-content/themes/dclmn-newsmatic/partials/home-boxes.php
+++ wp-content/themes/dclmn-newsmatic/partials/home-boxes.php
@@ -1,4 +1,17 @@
-<div class="dclmn-mobile-header-open" aria-expanded="false"><span>menu <img src="<?php echo get_stylesheet_directory_uri() ?>/images/hamburger.png" class="hamburger"></span></div>
+<div class="dclmn-mobile-header-open" aria-expanded="false">
+  <span>
+    menu
+    <svg viewBox="0 0 24.00 24.00" xmlns="http://www.w3.org/2000/svg" transform="matrix(1, 0, 0, -1, 0, 0)rotate(0)">
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+      <g id="SVGRepo_iconCarrier">
+        <path d="M4 18L20 18" stroke-width="2.4" stroke-linecap="round"></path>
+        <path d="M4 12L20 12" stroke-width="2.4" stroke-linecap="round"></path>
+        <path d="M4 6L20 6" stroke="#fff" stroke-width="2.4" stroke-linecap="round"></path>
+      </g>
+    </svg>
+  </span>
+</div>
 
 <div class="home-boxes">
   <div>
