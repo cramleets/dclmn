367b7c3e9e440d8985e7547e29c277812d90eba3
msteel <msteel@napco.com>
2026-04-24 11:05:36 -0400

    auths

--- wp-content/themes/dclmn-newsmatic/partials/cp-nav.php
+++ wp-content/themes/dclmn-newsmatic/partials/cp-nav.php
@@ -2,7 +2,10 @@
   <a href="<?php echo home_url('cp/') ?>" class="button">Dashboard</a>
   <a href="<?php echo home_url('cp/cps/') ?>" class="button">CPs</a>
   <a href="<?php echo home_url('cp/leadership/') ?>" class="button">Leadership</a>
-  <a href="<?php echo home_url('cp/dclmn-contacts/') ?>" class="button">Contacts</a>
+  <?php //if (dclmn_user_is_exec()): ?>
+  <?php if (current_user_can('update_core')): ?>
+    <a href="<?php echo home_url('cp/dclmn-contacts/') ?>" class="button">Contacts</a>
+  <?php endif; ?>
   <?php if (current_user_can('edit_others_posts')): ?>
     <a href="<?php echo home_url('cp/precinct-voters/') ?>" class="button">My Voters</a>
   <?php endif; ?>
