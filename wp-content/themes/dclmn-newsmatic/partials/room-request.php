2198c9959c9e448bd0544072fbfd38dcdbc85717
msteel <msteel@napco.com>
2026-07-09 23:00:56 -0400

    Do the form.

--- wp-content/themes/dclmn-newsmatic/partials/room-request.php
+++ wp-content/themes/dclmn-newsmatic/partials/room-request.php
@@ -4,5 +4,5 @@ $dclmn_user = dclmn_get_user();
 ?>
 <?php if (is_object($dclmn_user) && dclmn_user_is_exec()): ?>
   <?php get_template_part('partials/cp-nav'); ?>
-  FORM GOES HERE
+  <?php echo do_shortcode('[formidable id=5]'); ?>
 <?php endif; ?>
