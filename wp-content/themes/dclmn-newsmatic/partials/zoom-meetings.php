54dc56cc70749d6ca8e5cc053fad3609bb8b0e3c
msteel <msteel@napco.com>
2026-07-28 08:35:52 -0400

    Date boxes for Zoom meeting list.

--- wp-content/themes/dclmn-newsmatic/partials/zoom-meetings.php
+++ wp-content/themes/dclmn-newsmatic/partials/zoom-meetings.php
@@ -5,31 +5,53 @@ $meetings = $zoom->get_meetings();
 $webinars = $zoom->get_webinars();
 
 $out = '';
-$out .= '<div class="zoom-meetings">';
+$out .= '<div class="zoom-meetings dclmn-events">';
 foreach ($meetings as $meeting) {
   if (!dclmn_auth('exec') && stristr($meeting['topic'], 'exec')) continue;
-
   $dt = new DateTime($meeting['start_time']);
   $dt->setTimezone(new DateTimeZone($meeting['timezone']));
 
-  $meeting_date = $dt->format('F j, Y');
+  $meeting_date = $dt->format('l, F j, Y');
   $meeting_time = $dt->format('g:i a');
 
-  $out .= '<li>';
+  $event_week_day         = $dt->format('l');
+  $event_week_day_short   = $dt->format('D');
+  $event_week_day_shorter = substr($event_week_day_short, 0, 2);
+
+  $event_day_num          = $dt->format('j');
+  $event_month            = $dt->format('F');
+  $event_month_short      = $dt->format('M');
+  $event_date_attr        = $dt->format('Y-m-d');
+  $event_time_short       = $dt->format('g:i a');;
+
+
+  $out .= '<div class="dclmn-event">';
+  $out .= '<span class="dclmn-event-flex">';
+  $out .= '<span>'; // date box flex item
+  $out .= '<span class="date-box">';
+  $out .= '<span class="date-box-dow">' . $event_month_short . '</span>';
+  $out .= '<span class="date-box-date">' . $event_day_num . '</span>';
+  $out .= '<span class="date-box-time">' . $event_time_short . '</span>';
+  $out .= '</span>'; // /date box
+  $out .= '</span>'; // /date box flex item
+
+  $out .= '<span>'; // date links flex item
+
   $out .= '<a href="' .  $meeting['join_url'] . '" target="_blank">';
-  $out .= '<strong>'. $meeting['topic'] .'</strong>';
+  $out .= '<strong>' . $meeting['topic'] . '</strong>';
   $out .= '</a>';
-  $out .= '<ul>';
-  $out .= '<li><strong>Date</strong> ' . $meeting_date . ' at '. $meeting_time .'</li>';
-  $out .= '<li><strong>Duration</strong> ' . $meeting['duration'] . ' minutes</li>';
-  $out .= '<li><strong>Meeting ID</strong> ' . $meeting['id'] . '</li>';
-  $out .= '</ul>';
-  $out .= '</li>';
+  $out .= '<br>' . $meeting_date . ' at ' . $meeting_time;
+  //$out .= '<br>&bull; <strong>Duration</strong> ' . $meeting['duration'] . ' minutes';
+  // $out .= '<br><strong>Meeting ID</strong> ' . $meeting['id'];
+  $out .= '<br><a href="' .  $meeting['join_url'] . '" target="_blank"><strong>Click Here to Join</strong></a>';
+  $out .= '</span>';
+  $out .= '</span>'; // /date links flex item
+  $out .= '</div>';
 }
 $out .= '</div>';
 
 if (!empty($meetings)) {
-  $out = '<h3>Upcoming Zoom Meetings</h3><ul>'. $out .'</ul>';
+  $out = '<h3>Upcoming Zoom Meetings</h3>' . $out . '';
 }
 
 echo $out;
