00187b2d99aee19e3d2392c83032321b17606c32
msteel <msteel@napco.com>
2026-08-11 09:46:49 -0400

    Allow public documents.

--- wp-content/themes/dclmn-newsmatic/single-document.php
+++ wp-content/themes/dclmn-newsmatic/single-document.php
@@ -1,4 +1,5 @@
 <?php
+global $post;
 
 //if the resource is not published then die.
 if ('publish' != $post->post_status) {
@@ -6,17 +7,19 @@ if ('publish' != $post->post_status) {
 }
 
 //if the resource is gated then send them back to the gate
-if (! (dclmn_auth('cp') || current_user_can('edit_others_posts'))) {
-  $out = '';
-  $out .= '<title>DCLMN | Forbidden File</title>';
-  $out .= '<div style="text-align: center; font-family: tahoma;">';
-  $out .= '<br>';
-  $out .= '<p><a href="'. home_url() .'"><img src="'. get_stylesheet_directory_uri() .'/images/dclmn-alt-3.png" style="height: 160px; display: inline-block;"></a></p>';
-  $out .= '<h2>You do not have access to this document.</h2>';
-  $out .= '<p><a href="'. home_url() .'" style="text-decoration: none;">DCLMN.us</a></p>';
-  $out .= '</div>';
-  echo $out;
-  exit;
+if (empty($post->open_to_the_public) || !$post->open_to_the_public) {
+  if (! (dclmn_auth('cp') || current_user_can('edit_others_posts'))) {
+    $out = '';
+    $out .= '<title>DCLMN | Forbidden File</title>';
+    $out .= '<div style="text-align: center; font-family: tahoma;">';
+    $out .= '<br>';
+    $out .= '<p><a href="'. home_url() .'"><img src="'. get_stylesheet_directory_uri() .'/images/dclmn-alt-3.png" style="height: 160px; display: inline-block;"></a></p>';
+    $out .= '<h2>You do not have access to this document.</h2>';
+    $out .= '<p><a href="'. home_url() .'" style="text-decoration: none;">DCLMN.us</a></p>';
+    $out .= '</div>';
+    echo $out;
+    exit;
+  }
 }
 
 $post = dclmn_get_post(get_the_ID());
